edd1012062f02383f55906486ca76d3692325588
quickjkee <jkee2013@mail.ru>
2025-05-21 09:56:23 -0700

    diversity

--- run.sh
+++ run.sh
@@ -10,38 +10,35 @@ accelerate launch --num_processes=8 --multi_gpu --mixed_precision fp16 --main_pr
     --pretrained_model_name_or_path=$MODEL_NAME \
     --train_dataloader_config_path=$DATASET_PATH \
     --current_task="validate_teacher" \
-    --text_column="text" \
-    --image_column="image" \
+    --output_dir="results" \
+    --seed=42 \
     --train_batch_size=2 \
-    --gradient_checkpointing \
-    --checkpointing_steps=5000 \
+    --eval_batch_size=4 \
     --learning_rate=2e-6 \
-    --num_boundaries=6 \
-    --scales="32,48,64,80,96,128" \
-    --do_gan_loss \
-    --do_pixels_downscale \
-    --stochastic_case \
-    --num_discriminator_upds=3 \
-    --num_discriminator_layers=4 \
     --lr_scheduler="constant_with_warmup" \
     --lr_warmup_steps=300 \
-    --seed=42 \
-    --output_dir="results" \
-    --rank=64 \
+    --do_gan_loss \
+    --num_discriminator_upds=3 \
+    --num_discriminator_layers=4 \
     --cls_blocks=11 \
     --pdm_blocks=22 \
     --cfg_teacher=3.5 \
-    --cfg_fake=4.5 \
+    --cfg_fake=3.5 \
+    --rank=64 \
     --apply_lora_to_attn_projections \
     --apply_lora_to_mlp_projections \
     --validation_steps=20 \
     --evaluation_steps=10 \
+    --max_train_steps=1000 \
+    --checkpointing_steps=5000 \
+    --max_eval_samples=1000 \
+    --calc_diversity \
+    --resume_from_checkpoint=latest \
+    --gradient_checkpointing \
+    --text_column="text" \
+    --image_column="image" \
     --coco_ref_stats_path stats/fid_stats_mscoco256_val.npz \
     --inception_path stats/pt_inception-2015-12-05-6726825d.pth \
-    --max_train_steps=1000 \
-    --resume_from_checkpoint=latest \
-    --max_eval_samples=5000 \
-    --eval_batch_size=4 \
     --pickscore_model_name_or_path yuvalkirstain/PickScore_v1 \
     --clip_model_name_or_path laion/CLIP-ViT-H-14-laion2B-s32B-b79K \
     # --offload_text_encoders \
